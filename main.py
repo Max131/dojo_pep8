facdcc2c8450b1406a54a0cbf6a6e1ee292d6624
Mario <31197372+Max131@users.noreply.github.com>
2023-05-27 14:54:42 -0600

    Fix: format/identation

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from codebreaker import Codebreaker
 
-intentos_totales = 10
+INTENTOS_TOTALES = 10
 codebreaker = Codebreaker()
 
 intento = 0
 
-print('Jugar Codebreaker!')
+print("Jugar Codebreaker!")
 
-while intento != intentos_totales:
-   number = input('Numero:');
-   resolve = codebreaker.adivinar(number)
-   print(resolve)
-   if resolve == True:
-      print('You win!!')
-      break
-
-
+while intento != INTENTOS_TOTALES:
+    number = input("Numero: ")
+    resolve = codebreaker.adivinar(number)
+    print(resolve)
+    if resolve is True:
+        print("You win!!")
+        break
